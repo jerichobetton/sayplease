fb307d3d82ce584e1f439685e16b60c399efb0b3
Jericho Betton <jerichobetton@gmail.com>
2023-11-08 18:17:44 -0500

    Adding to git repo

---
+++ script.js
@@ -0,0 +1,9 @@
+//Pop up with beggining text
+alert("Who dares up wake me up from my slumber!?! Go away!");
+let userCmd = prompt("What do you have to say that's so important??")
+
+let isFast = confirm("Are you fast?");
+alert(isFast);
+// if "cancel"/false, give another message along the lines of "Oh so now you wanna leave?"
+
+//Noel was here :P
